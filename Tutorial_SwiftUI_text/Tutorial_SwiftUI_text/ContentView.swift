dae2a298e372e0a1c0e2e48435708941e357b84f
song1900 <woojin1900@gmail.com>
2021-06-18 17:16:25 +0900

    Create Tutorial_SwiftUI_text

---
+++ Tutorial_SwiftUI_text/Tutorial_SwiftUI_text/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial_SwiftUI_text
+//
+//  Created by 송우진 on 2021/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
