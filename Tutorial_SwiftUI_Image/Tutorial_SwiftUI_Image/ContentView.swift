95274234fe388ca430ba0b61db5058ecb05f31e1
song1900 <woojin1900@gmail.com>
2021-06-19 20:49:18 +0900

    Create Tutorial_SwiftUI_Image

---
+++ Tutorial_SwiftUI_Image/Tutorial_SwiftUI_Image/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial_SwiftUI_Image
+//
+//  Created by 송우진 on 2021/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
